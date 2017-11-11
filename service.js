d5600bb38e78c394ae0d75c09631fabce3d31ed4
Clare Liguori <liguori@amazon.com>
2017-11-11 14:33:04 -0800

    Add caching via Redis

--- service.js
+++ service.js
@@ -8,6 +8,55 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
+const USE_CACHE = process.env.USE_CACHE;
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+
+function createCache() {
+  if (USE_CACHE) {
+    var client = require('redis').createClient(
+      REDIS_PORT,
+      REDIS_HOST,
+      {
+        connect_timeout: 500,
+      });
+
+    client.on('error', function (err) {
+      throw err;
+    });
+
+    var cache = require('express-redis-cache')({
+      client: client,
+      expire: 60
+    });
+
+    cache.on('message', function(message){
+      console.log("cache", message);
+    });
+
+    cache.on('error', function(error){
+      console.error("cache", error);
+    });
+
+    return cache;
+  } else {
+    return null;
+  }
+}
+const cache = createCache();
+
+function cacheRoute() {
+  if (cache) {
+    return cache.route();
+  } else {
+    // no-op
+    return function(req, res, next) {
+      next();
+    }
+  }
+}
+
 function parseA(req) {
   return numeral(req.query.a).value();
 }
@@ -31,28 +80,28 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.get('/add', function (req, res) {
+app.get('/add', cacheRoute(), function (req, res) {
   const a = parseA(req);
   const b = parseB(req);
   const value = calc.add(a, b);
   sendResult(value, res);
 })
 
-app.get('/subtract', function (req, res) {
+app.get('/subtract', cacheRoute(), function (req, res) {
   const a = parseA(req);
   const b = parseB(req);
   const value = calc.subtract(a, b);
   sendResult(value, res);
 })
 
-app.get('/multiply', function (req, res) {
+app.get('/multiply', cacheRoute(), function (req, res) {
   const a = parseA(req);
   const b = parseB(req);
   const value = calc.multiply(a, b);
   sendResult(value, res);
 })
 
-app.get('/divide', function (req, res) {
+app.get('/divide', cacheRoute(), function (req, res) {
   const a = parseA(req);
   const b = parseB(req);
   const value = calc.divide(a, b);
@@ -60,6 +109,11 @@ app.get('/divide', function (req, res) {
 })
 
 if (!module.parent) {
-  app.listen(PORT, HOST);
+  var server = app.listen(PORT, HOST);
+  server.on('close', function () {
+    if (cache) {
+      cache.client.end(true);
+    }
+  });
 }
-module.exports = app;
+module.exports = { app, cache };
--- test/service_spec.js
+++ test/service_spec.js
@@ -4,13 +4,33 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 const expect = chai.expect;
-const app = require('../service');
+const service = require('../service');
+const app = service.app;
+const cache = service.cache;
 
 chai.use(chaiHttp);
 
 describe('Calculator service', () => {
 
   var server;
+
+  it('it should connect to the redis cache', (done) => {
+    if (cache) {
+      cache.on("connected", function() {
+        done();
+      });
+
+      if (cache.client.connected) {
+        done();
+      }
+
+      cache.on("error", function (err) {
+        assert.fail(err, null);
+      });
+    } else {
+      done();
+    }
+  });
 
   it('it should add two numbers', (done) => {
     server = app.listen();
@@ -28,6 +48,9 @@ describe('Calculator service', () => {
 
   after(function(done) {
     if (server) {
+      if (cache && cache.client.connected) {
+        cache.client.end(true);
+      }
       server.close(done);
     }
   });
